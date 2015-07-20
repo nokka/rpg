bc2c18802485559ae0ce61768924e3ae252feb75
Björn Wallentin <devnull@localhost>
2015-07-20 16:00:21 +0200

    More inventory work

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Inventory : MonoBehaviour {
 
@@ -11,6 +12,11 @@ public class Inventory : MonoBehaviour {
 
     private bool showInventory;
     private ItemDatabase database;
+    private bool showToolTip = false;
+    private string toolTip;
+    private bool draggingItem = false;
+    private Item draggedItem;
+    private int prevIndex;
 
 	// Use this for initialization
 	void Start () {
@@ -18,11 +24,14 @@ public class Inventory : MonoBehaviour {
         for (int i = 0; i < (slotsX * slotsY); i++)
         {
             slots.Add(new Item());
+            inventory.Add(new Item());
         }
 
         database = GameObject.FindGameObjectWithTag("Item Database").GetComponent<ItemDatabase>();
-        inventory.Add(database.items[0]);
-        inventory.Add(database.items[1]);
+
+        // Add all items we have to the inventory
+        foreach (var x in database.items)
+            AddItem(x.ItemID);
 	}
 
     void Update()
@@ -34,24 +43,140 @@ public class Inventory : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Add an item to the inventory
+    /// </summary>
+    /// <param name="id"></param>
+    void AddItem(int id)
+    {
+        //TODO: Snygga till den här koden
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            if (inventory[i].ItemName == null)
+            {
+                for (int j = 0; j < database.items.Count; j++)
+                {
+                    if (database.items[j].ItemID == id)
+                    {
+                        inventory[i] = database.items[j];
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Removes the first item given an ID from the inventory
+    /// </summary>
+    /// <param name="id"></param>
+    void RemoveItem(int id)
+    {
+        var item = inventory.FirstOrDefault(s => s.ItemID == id);
+
+        if (item != null)
+        {
+            if (item.ItemID != null)
+                inventory[item.ItemID] = new Item();
+        }
+    }
+
+    /// <summary>
+    /// Does the inventory contain an item with the given ID?
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    bool InventoryContains(int id)
+    {
+        return inventory.Any(x => x.ItemID == id);
+    }
+
     void OnGUI()
     {
+        toolTip = "";
         GUI.skin = skin;
 
         if (showInventory)
         {
             DrawInventory();
+
+            if (showToolTip)
+                GUI.Box(new Rect(Event.current.mousePosition.x + 15f, Event.current.mousePosition.y, 200, 200), toolTip, skin.GetStyle("Tooltip"));
+        }
+
+        if(draggingItem)
+            GUI.DrawTexture(new Rect(Event.current.mousePosition.x, Event.current.mousePosition.y, 50, 50), draggedItem.ItemIcon);
+    }
+
+    /// <summary>
+    /// Draw all the shit on the screen for the Inventory
+    /// </summary>
+    void DrawInventory()
+    {
+        Event e = Event.current;
+        int i = 0;
+        for (int y = 0; y < slotsY; y++)
+        {
+            for (int x = 0; x < slotsX; x++)
+            {
+                Rect slotRect = new Rect(x * 60, y * 60, 50, 50);
+                GUI.Box(slotRect, "", skin.GetStyle("Slot"));
+                slots[i] = inventory[i];
+
+                //ItemName will be null if it doesn't exist
+                if (slots[i].ItemName != null)
+                {
+                    GUI.DrawTexture(slotRect, slots[i].ItemIcon);
+
+                    if (slotRect.Contains(e.mousePosition))
+                    {
+                        toolTip = CreateToolTip(slots[i]);
+                        showToolTip = true;
+
+                        // Item being dragged around in the inventory
+                        if (e.button == 0 && e.type == EventType.mouseDrag && !draggingItem)
+                        {
+                            draggingItem = true;
+                            prevIndex = i;
+                            draggedItem = slots[i];
+                            inventory[i] = new Item(); //Empty inventory slot
+                        }
+
+                        // Item is being dropped in the inventory
+                        if (e.type == EventType.mouseUp && draggingItem)
+                        {
+                            inventory[prevIndex] = inventory[i];
+                            inventory[i] = draggedItem;
+                            draggingItem = false;
+                            draggedItem = null;
+                        }
+                    }
+                }
+                else 
+                {
+                    if (slotRect.Contains(e.mousePosition))
+                    {
+                        if (e.type == EventType.mouseUp && draggingItem)
+                        {
+                            inventory[i] = draggedItem;
+                            draggingItem = false;
+                            draggedItem = null;
+                        }
+                    }
+                }
+
+                // Hide tooltip
+                if (toolTip == "")
+                    showToolTip = false;
+
+                i++;
+            }
         }
     }
 
-    void DrawInventory()
+    string CreateToolTip(Item item)
     {
-        for (int x = 0; x < slotsX; x++)
-        {
-            for (int y = 0; y < slotsY; y++)
-            {
-                GUI.Box(new Rect(x * 60, y * 60, 50, 50), "", skin.GetStyle("Slot"));
-            }
-        }
+        toolTip = "<color=#4DA4BF>" + item.ItemName + "</color>\n\n" + "<color=#F2F2F2>" + item.ItemDescription + "</color>";
+        return toolTip;
     }
 }
--- Assets/Scripts/Inventory/ItemDatabase.cs
+++ Assets/Scripts/Inventory/ItemDatabase.cs
@@ -9,7 +9,7 @@ public class ItemDatabase : MonoBehaviour {
     void Start()
     {
         items.Add(new Item("Apple", 0, "Best Apple EU", 0, 0, Item.ItemTypes.Consumable));
-        items.Add(new Item("Shirt", 0, "Fancy Pants Shirt", 0, 0, Item.ItemTypes.Weapon));
-        items.Add(new Item("Sword", 0, "Bronze Sword of Doom", 1, 2, Item.ItemTypes.Weapon));
+        items.Add(new Item("Shirt", 1, "Fancy Pants Shirt", 0, 0, Item.ItemTypes.Weapon));
+        items.Add(new Item("Sword", 2, "Bronze Sword of Doom", 1, 2, Item.ItemTypes.Weapon));
     }
 }
